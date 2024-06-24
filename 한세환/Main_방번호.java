d95fb8906448a839086a70b50648e78ad60c0846
Sehwan <abovenormal5023@gmail.com>
2024-06-24 23:53:42 +0900

    한세환 백준_방번호_0624

---
+++ 한세환/Main_방번호.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] arr = new int[10];
+        for (int i = 0; i < s.length(); i++) {
+            int num = Character.getNumericValue(s.charAt(i));
+            if (num == 6) {
+                arr[9]++;
+            } else {
+                arr[num]++;
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int nine = arr[9] / 2;
+        if (arr[9] % 2 == 1) nine++;
+        max = Math.max(max, nine);
+        System.out.print(max);
+    }
+}
