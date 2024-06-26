4b03421f0c79037368219316951036d4e36d03c3
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-06-26 20:20:39 +0900

    0626 서상원

---
+++ 서상원/백준 LIS.py
@@ -0,0 +1,10 @@
+n=int(input())
+data=list(map(int,input().split()))
+dp=[1 for _ in range(n)]
+
+for i in range(1,n):
+    for j in range(i):
+        if data[i] > data[j] :
+            dp[i]=max(dp[i],dp[j]+1)
+
+print(max(dp))
